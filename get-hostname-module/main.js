50c5609314598d9d6a2074caee0fdde3749f48b6
nasirhm <nasirhussainm14@gmail.com>
2018-10-21 13:58:45 -0500

    complete get hostname module

--- get-hostname-module/main.js
+++ get-hostname-module/main.js
@@ -1,9 +1,11 @@
 var dns = require('dns');
 
-dns.lookup('https://wikipedia.com',function onLookup(err, address, family) {
-    console.log("Address", address);
+dns.lookup('www.wikipedia.org',function onLookup(err, address, family) {
+    console.log("Address : ", address);
     dns.reverse(address, function (err, hostnames) {
-        
-    })
-
-})
+        if(err){
+            console.log(err.stack);
+        }
+        console.log("Reserve for Address : "+address+" : "+JSON.stringify(hostnames));
+    });
+});
