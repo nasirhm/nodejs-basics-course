52270c8a8427f632ac2727c96246fbaac901bb3d
Nasir Hussain <nasirhussainm14@gmail.com>
2018-10-25 04:05:57 +0500

    assertion complete

---
+++ assertion-example/main.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+function add(a,b) {
+    return a+b;
+}
+
+var value = add(1,2);
+/*asserting with strict
+assert(value===3,"1 and 2 = 3");
+//equivalent to
+assert.strictEqual(value,3,"1 and 2 = 3");
+*/
+
+/* simple equal asserting 
+assert(value == 3,"1 and 2 = 3");
+//equivalent to
+assert.equal(value,3,"1 and 2 = 3");
+*/
+
+/** Asserting Not Equals 
+assert(value!==2,"1 + 2 != 3");
+//equivalent to 
+assert.notEqual(value,2,"1 + 2 != 3");
+*/
